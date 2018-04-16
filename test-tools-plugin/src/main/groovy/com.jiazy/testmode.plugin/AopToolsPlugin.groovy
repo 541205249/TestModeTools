d5c6327386cbc8533fb2c757cb1adede153c4ae9
li_xiao_feng <18813293916@163.com>
2018-04-16 15:56:56 +0800

    1、修复插件版本不正确的问题

--- test-tools-plugin/src/main/groovy/com.jiazy/testmode.plugin/AopToolsPlugin.groovy
+++ test-tools-plugin/src/main/groovy/com.jiazy/testmode.plugin/AopToolsPlugin.groovy
@@ -28,9 +28,9 @@ class AopToolsPlugin implements Plugin<Project>{
         }
 
         project.dependencies {
-            debugCompile 'com.jiazy.testmode:aop-tools-runtime:0.1.29'
+            debugCompile 'com.jiazy.testmode:aop-tools-runtime:0.1.32'
             debugCompile 'org.aspectj:aspectjrt:1.8.13'
-            compile 'com.jiazy.testmode:annotation:0.1.28'
+            compile 'com.jiazy.testmode:annotation:0.1.32'
         }
 
         variants.all { variant ->
